fae0abdfd4711cb673591e30c2dea2db40a2f39f
Naddiya <nadiamedkouriz@gmail.com>
2022-06-26 09:25:05 +0200

    feat: setup theme

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { unregister } from './core/service-worker/serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { GlobalStyles, theme } from './styles';
+import { ThemeProvider } from 'styled-components';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
 	<React.StrictMode>
-		<div>Hello </div>
+		<ThemeProvider theme={theme}>
+			<GlobalStyles />
+			<div>Hello </div>
+		</ThemeProvider>
 	</React.StrictMode>
 );
 
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,14 +1,2 @@
-import { createGlobalStyle } from 'styled-components';
-
-export default createGlobalStyle`
-html {
-  height: 100%;
-
-  body: {
-    display: flex; 
-    flex-direction: column;
-    height: 100%;
-    margin: 0;
-  }
-}
-`;
+export { default as GlobalStyles } from './global';
+export { default as theme } from './theme';
