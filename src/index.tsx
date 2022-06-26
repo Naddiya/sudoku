e2bfee330122e7dde9c5a4b6c029901396ec927f
Naddiya <nadiamedkouriz@gmail.com>
2022-06-26 12:20:30 +0200

    feat: grid style

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { unregister } from './core/service-worker/serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import { GlobalStyles, theme } from './styles';
 import { ThemeProvider } from 'styled-components';
+
+import { unregister } from './core/service-worker/serviceWorkerRegistration';
+import { GlobalStyles, theme } from './styles';
+import { Content, Title, Card, Grid } from './components';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
@@ -12,7 +14,12 @@ root.render(
 	<React.StrictMode>
 		<ThemeProvider theme={theme}>
 			<GlobalStyles />
-			<div>Hello </div>
+			<Content data-cy='content'>
+				<Title data-cy='title'>Sudoku</Title>
+				<Card data-cy='card'>
+					<Grid />
+				</Card>
+			</Content>
 		</ThemeProvider>
 	</React.StrictMode>
 );
